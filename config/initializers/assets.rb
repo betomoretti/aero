7f4469716bdaf419f4b5ce2b759c3bcf802c8ee2
Pc de test <guido.collino@aerolaplata.com>
2014-07-30 16:37:11 -0300

    Index realizado. Faltan arreglar algunos estilos

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,8 +4,10 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( easydropdown.flat.css )
 Rails.application.config.assets.precompile += %w( responsive.css )
+Rails.application.config.assets.precompile += %w( styles.css )
 Rails.application.config.assets.precompile += %w( minified/jquery-1.9.1.min.js )
 Rails.application.config.assets.precompile += %w( minified/jquery.easydropdown.min.js )
+Rails.application.config.assets.precompile += %w( minified/jquery.backstretch.min.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )

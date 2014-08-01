daf21936def177970720fba93e70876846b15956
Mariano Moretti <marianomoretti.87@gmail.com>
2014-08-01 03:07:13 -0300

    Arreglos de merge

--- app/models/unique_destination.rb
+++ app/models/unique_destination.rb
@@ -1,3 +1,4 @@
 class UniqueDestination < ActiveRecord::Base
     belongs_to :unique_zone
+    has_many :programs, :class_name => "Program", :foreign_key => "unique_destination_id"
 end
--- app/models/unique_zone.rb
+++ app/models/unique_zone.rb
@@ -1,3 +1,7 @@
 class UniqueZone < ActiveRecord::Base
     has_many :unique_destinations, :dependent => :destroy
+
+    def programs 
+        Program.find(:all, :conditions => [ "unique_destination_id IN (?)", self.unique_destinations])
+    end
 end

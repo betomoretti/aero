242ffab9ab160bad029f59c5e1d54ece79d792df
Pc de test <guido.collino@aerolaplata.com>
2014-08-19 12:42:24 -0300

    Version de prueba con imagen estatica obtenida de amazon con un metodo en la entidad Program

--- app/models/program.rb
+++ app/models/program.rb
@@ -4,4 +4,7 @@ class Program < ActiveRecord::Base
   scope :uniques, -> { where(category_program_id: CategoryProgram.unique.id)}
   scope :search_uniques, ->(param) {uniques.joins(unique_destination: :unique_zone).where('unique_destinations.name LIKE ? OR unique_zones.name LIKE ?', "%#{param}%", "%#{param}%")}
 
+  def temp_photo
+  	"http://s3.amazonaws.com/aerolaplata/program_category_photos/8/SOL_Y_PLAYA.png"
+  end
 end

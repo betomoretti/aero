01b877b24e36c9feddf7f8802916158798ac8c81
Pc de test <guido.collino@aerolaplata.com>
2014-08-29 09:37:00 -0300

    algunos cambios

--- app/models/program_category_photo.rb
+++ app/models/program_category_photo.rb
@@ -1,10 +1,4 @@
 class ProgramCategoryPhoto < ActiveRecord::Base
   	belongs_to :category_program
-
-    has_attached_file :download,
-                    :storage => :s3,
-                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
-    validates_as_attachment
-
     
 end

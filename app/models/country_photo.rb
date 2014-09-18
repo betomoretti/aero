8d8211e16ae0389b0e14960628944ea717a25af2
Pc de test <guido.collino@aerolaplata.com>
2014-09-18 12:29:46 -0300

    Informacion por pais

--- app/models/country_photo.rb
+++ app/models/country_photo.rb
@@ -1,13 +1,12 @@
 class CountryPhoto < ActiveRecord::Base
 	belongs_to :country
 
-	def photo_thumb
-		elem = CountryPhoto.select("filename").where(parent_id: self.id.to_s, thumbnail: "thumbnail").first_or_initialize
-		@string = "http://s3.amazonaws.com/aerolaplata-dev/country_photos/"<<self.id.to_s<<"/"<<elem.filename	  							
+	def photo
+		@string = "http://s3.amazonaws.com/aerolaplata-dev/country_photos/"<<self.id.to_s<<"/"<<self.filename	  							
 	end
 
-	def photo_header
-		elem = CountryPhoto.select("filename").where(parent_id: self.id.to_s, thumbnail: "header").first_or_initialize
+	def photo_thumb
+		elem = CountryPhoto.select("filename").where(parent_id: self.id.to_s, thumbnail: "thumbnail").first_or_initialize
 		@string = "http://s3.amazonaws.com/aerolaplata-dev/country_photos/"<<self.id.to_s<<"/"<<elem.filename	  							
 	end
 end

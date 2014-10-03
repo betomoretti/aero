9358610e16c25b17e8a6be95572a414e8e33d0da
Pc de test <guido.collino@aerolaplata.com>
2014-10-03 12:13:51 -0300

    Cambios para que tome los paises mas alla de que no tenga imagen. Se modifico el html que usa el generador de pdf para qeu tome los enters

--- app/models/country.rb
+++ app/models/country.rb
@@ -4,6 +4,6 @@ class Country < ActiveRecord::Base
 	has_many :programs, through: :areas
     has_one :country_photo
    
-    scope :unique_countries, -> { joins(:programs, :country_photo) }
+    scope :unique_countries, -> { joins(:programs) }
     scope :filter_by_name, -> (param) { joins(:programs).where("countries.name LIKE ?", "#{param}%").uniq}
 end

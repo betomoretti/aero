742fe41ec7ff047a79767317a52b980f4a1c9230
Pc de test <guido.collino@aerolaplata.com>
2014-10-06 11:35:20 -0300

    Cambios en la configuracion del mailer.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com.ar',
+    domain:               'gmail.com',
     user_name:            'aerolaplata.web',
     password:             'aero1234',
     authentication:       'plain',

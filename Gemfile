1b018911bce52c9bcf591c9930aa17eae9f48e36
Pc de test <guido.collino@aerolaplata.com>
2014-12-22 12:13:01 -0300

    Cambios en gem file. En initiliazer de wicket. Y en gitignore

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ gem 'wkhtmltopdf'
 gem 'wkhtmltopdf-binary'
 gem 'wicked_pdf'
 
+gem 'therubyracer', group: :production
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
-WickedPdf.config = {
-  :wkhtmltopdf => '/usr/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  :exe_path => '/usr/bin/wkhtmltopdf'
-}
+if Rails.env.production?
+    WickedPdf.config[:wkhtmltopdf] = '/usr/local/bin/wkhtmltopdf'
+else
+    WickedPdf.config[:wkhtmltopdf] = '/usr/bin/wkhtmltopdf'
+end
